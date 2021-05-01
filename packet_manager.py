796be1580fbe6e6fe4256841a40bd3abd6b3173d
Student <margarit-mikh@mail.ru>
2021-05-01 11:10:02 +0300

    Create packet manager

---
+++ packet_manager.py
@@ -0,0 +1,24 @@
+import json
+
+
+def packet(packet_type, payload):
+    return {
+        "type": packet_type,
+        "payload": payload
+    }
+
+
+def serialize(data):
+    return bytes(json.dumps(data), "utf8")
+
+
+def unserialize(buffer):
+    return json.loads(buffer.decode("utf8"))
+
+
+def message_packet(text):
+    return serialize(
+        packet("message", {
+            "text": text
+        })
+    )
